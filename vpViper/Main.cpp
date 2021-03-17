878484d2ac1326d36aa99be6a6d1fb1643fe842e
SomayehNorouzi <somayehnorouzi.gh@gmail.com>
2021-03-16 23:49:55 -0400

    License

--- vpViper/Main.cpp
+++ vpViper/Main.cpp
@@ -1,3 +1,49 @@
+// Eye-To-Hand VPC
+
+/****************************************************************************
+* Somayeh Norouzi:
+"This project provides the implementation of Eye-To-Hand VPC on an ablation
+catheter as a continuum robot.
+CRSimulation include the kinematic model of CR.
+
+The program is based on ViSP libraries."
+
+* ViSP, open source Visual Servoing Platform software.
+* Copyright (C) 2005 - 2019 by Inria. All rights reserved.
+*
+* This software is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* See the file LICENSE.txt at the root directory of this source
+* distribution for additional information about the GNU GPL.
+*
+* For using ViSP with software that can not be combined with the GNU
+* GPL, please contact Inria about acquiring a ViSP Professional
+* Edition License.
+*
+* See http://visp.inria.fr for more information.
+*
+* This software was developed at:
+* Inria Rennes - Bretagne Atlantique
+* Campus Universitaire de Beaulieu
+* 35042 Rennes Cedex
+* France
+*
+* If you have questions regarding the use of this file, please contact
+* Inria at visp@inria.fr
+*
+* This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+* WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*
+* Description:
+* Interface for a  generic ADEPT Viper (either 650 or 850) robot.
+*
+* Authors:
+* Fabien Spindler
+*
+*****************************************************************************/
+
 #include "stdafx.h"
 #include <visp3/gui/vpDisplayGDI.h>
 #include <visp3/gui/vpDisplayOpenCV.h>
